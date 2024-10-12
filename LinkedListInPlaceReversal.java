6e5f0b5ea32bd73eaafd21e9ccf3f08ba1c92660
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-12 10:48:20 +0530

    Update LinkedListInPlaceReversal.java

--- LinkedListInPlaceReversal.java
+++ LinkedListInPlaceReversal.java
@@ -15,3 +15,26 @@ class Solution {
         return tmp;
     }
 }
+
+
+// 2. Flatten Binary Tree to Linked List
+
+
+class Solution {
+    public void flatten(TreeNode root) {
+
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+
+                TreeNode runner = curr.left;
+                while (runner.right != null) runner = runner.right;
+                
+                runner.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
+        }
+    }
+}
