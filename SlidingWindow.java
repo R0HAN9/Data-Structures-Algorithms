d65bb06088725ecfe7d1bb580671381e7f74d40d
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-13 11:42:42 +0530

    Update SlidingWindow.java

--- SlidingWindow.java
+++ SlidingWindow.java
@@ -1,3 +1,47 @@
+// Smallest Range Covering Elements from K Lists
+
+
+class Solution {
+    public int[] smallestRange(List<List<Integer>> nums) {
+        
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        int currentMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.size(); i++) {
+            minHeap.offer(new int[] {nums.get(i).get(0), i, 0});
+            currentMax = Math.max(currentMax, nums.get(i).get(0));
+        }
+
+        int[] smallRange = new int[] {0, Integer.MAX_VALUE};
+
+        while (true) {
+            int[] curr = minHeap.poll();
+            int currentMin = curr[0], listIdx = curr[1], elementIdx = curr[2];
+
+            if ((currentMax - currentMin < smallRange[1] - smallRange[0]) ||
+                (currentMax - currentMin == smallRange[1] - smallRange[0] && currentMin < smallRange[0])) {
+                    smallRange[0] = currentMin;
+                    smallRange[1] = currentMax;
+                }
+
+            if (elementIdx + 1 < nums.get(listIdx).size()) {
+
+                int nextVal = nums.get(listIdx).get(elementIdx + 1);
+                minHeap.offer(new int[] {nextVal, listIdx, elementIdx + 1});
+                currentMax = Math.max(currentMax, nextVal);
+            }
+            else {
+                break;
+            }
+        }
+
+        return smallRange;
+    }
+}
+
+
+
+
 // Longest Substring Without Repeating Characters
 
 class Solution {
