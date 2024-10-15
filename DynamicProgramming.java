7535eb792fbb9f637b4485dc7cb88ddc4df44a9a
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-15 10:49:00 +0530

    Update DynamicProgramming.java

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -13,3 +13,35 @@ class Solution {
         return Math.max(rob, norob);
     }
 }
+
+
+// Maximum Product Subarray
+
+class Solution {
+    int max = 1; int min = 1;
+    public int maxProduct(int[] nums) {
+        return dfs(0, nums);
+    }
+    public int dfs(int i, int[] nums) {
+        if (i >= nums.length - 1) {
+            max = nums[nums.length - 1]; min = max;
+            if (nums[nums.length - 1] == 0) {
+                min = 1; max = 1;
+            }
+            return nums[nums.length - 1] != 0 ? max : 0;
+        }
+        int res = dfs(i + 1, nums);
+        
+        if (nums[i] == 0) { min = 1; max = 1; 
+                 return res > 0 ? res : 0;
+        }
+        int temp = max;
+        max = Math.max(max * nums[i], min * nums[i]);
+        max = Math.max(max, nums[i]);
+        min = Math.min(temp * nums[i], min * nums[i]);
+        min = Math.min(min, nums[i]);
+        
+        res = Math.max(res, max);
+        return res;
+    }
+}
