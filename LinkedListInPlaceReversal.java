0d6e8aa5580d82d3bb0c3edfc3136bf8f7787256
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-17 10:46:30 +0530

    Update LinkedListInPlaceReversal.java

--- LinkedListInPlaceReversal.java
+++ LinkedListInPlaceReversal.java
@@ -1,3 +1,28 @@
+// Remove Nth Node From End of List
+
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+        ListNode result = new ListNode(0, head);
+        ListNode dummy = result;
+
+        for (int i = 0; i < n; i++) {
+            head = head.next;
+        }
+
+        while (head != null) {
+            head = head.next;
+            dummy = dummy.next;
+        }
+        dummy.next = dummy.next.next;
+
+        return result.next;
+    }
+}
+
+
+
 // Swap Nodes in Pairs
 
 class Solution {
