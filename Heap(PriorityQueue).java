26f04c4f9e9037beabef4c2947031eb73ff1cdb2
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-20 13:21:45 +0530

    Update Heap(PriorityQueue).java

--- Heap(PriorityQueue).java
+++ Heap(PriorityQueue).java
@@ -1,1 +1,38 @@
 // Top K Frequent Elements
+
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        List<Integer>[] bucket = new List[nums.length + 1];
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int num : nums) {
+            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int key : hashMap.keySet()) {
+            int freq = hashMap.get(key);
+
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
+        }
+
+        int[] ans = new int[k];
+        int pos = 0;
+
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            if (bucket[i] != null) {
+
+                for (int j = 0; j < bucket[i].size() && pos < k; j++) {
+                    ans[pos] = bucket[i].get(j);
+                    pos++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
