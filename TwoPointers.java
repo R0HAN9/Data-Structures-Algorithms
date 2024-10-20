5434450af04604f2a6b624405b5395cd9e5ccc1f
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-20 13:13:07 +0530

    Update TwoPointers.java

--- TwoPointers.java
+++ TwoPointers.java
@@ -95,3 +95,30 @@ class Solution {
         return right - left - 1;
     }
 }
+
+
+// Two Sum II - Input Array Is Sorted
+
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        
+        int[] ans = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            
+            if ((numbers[left] + numbers[right]) < target) left++;
+            else if ((numbers[left] + numbers[right]) > target) right--;
+
+            else {
+                ans[0] = left + 1;
+                ans[1] = right + 1;
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
