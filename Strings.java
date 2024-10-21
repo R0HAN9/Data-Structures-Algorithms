c391fe0d2a8a9cad5c1b40a830efb2f26722b545
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-21 10:57:03 +0530

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -1,1 +1,28 @@
 // Split a String Into the Max Number of Unique Substrings
+
+class Solution {
+    public int maxUniqueSplit(String s) {
+        return solve(0, s, new HashSet<>());
+    }
+
+    public int solve(int start, String s, Set<String> set) {
+        if (start == s.length()) {
+            return 0;
+        }
+
+        int maxSplits = 0;
+        for (int i = start + 1; i <= s.length(); i++) {
+            
+            String substring = s.substring(start, i);
+            if (!set.contains(substring)) {
+                set.add(substring);
+
+                int splits = 1 + solve(i, s, set);
+                maxSplits = Math.max(maxSplits, splits);
+                set.remove(substring);
+            }
+        }
+
+        return maxSplits;
+    }
+}
