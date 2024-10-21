adda9c705a0dbac4333cda08318ad00c6dfbc18b
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-21 10:42:28 +0530

    Update DynamicProgramming.java

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -76,3 +76,28 @@ class Solution {
         return prev[n - 1];
     }
 }
+
+
+// Word Break
+
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (String word : wordDict) {
+
+                int start = i - word.length();
+                if (start >= 0 && dp[start] && s.substring(start, i).equals(word)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
