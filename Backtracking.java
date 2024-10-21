9dd70ad99e6d3ee7af01c2f9da175ee05655fc50
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-21 10:35:24 +0530

    Update Backtracking.java

--- Backtracking.java
+++ Backtracking.java
@@ -143,3 +143,46 @@ class Solution {
         Helper(op, nums, startIndex + 1);
     }
 }
+
+
+
+// Word Search
+
+
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+
+                if (board[i][j] == word.charAt(0)) {
+                    boolean ans = search(board, word, i, j, 0);
+
+                    if (ans) return ans; 
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean search(char[][] board, String word, int row, int col, int index) {
+        if (index == word.length()) return true;
+
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != word.charAt(index)) {
+            return false;
+        }
+
+        board[row][col] = '*';
+        int[] rows = {-1, 1, 0, 0};
+        int[] cols = {0, 0, -1, 1};
+
+        for (int i = 0; i < cols.length; i++) {
+            boolean ans = search(board, word, row + rows[i], col + cols[i], index + 1);
+
+            if (ans == true) return ans;
+        }
+
+        board[row][col] = word.charAt(index);
+        return false;
+    }
+}
