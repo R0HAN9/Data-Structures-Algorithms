fe5fe6af5d716c9aa2526ffdd809cb15e1b03073
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-22 11:28:08 +0530

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -1,3 +1,51 @@
+
+// Basic Calculator
+
+
+class Solution {
+    public int calculate(String s) {
+        
+        int number = 0;
+        int signValue = 1;
+        int result = 0;
+
+        Stack<Integer> optStack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) number = number * 10 + (c - '0');
+            else if (c == '+' || c == '-') {
+
+                result += number * signValue;
+                signValue = (c == '-') ? -1 : 1;
+                number = 0;
+            }
+            else if (c == '(') {
+
+                optStack.push(result);
+                optStack.push(signValue);
+                result = 0;
+                signValue = 1;
+            }
+            else if (c == ')') {
+
+                result += signValue * number;
+                result *= optStack.pop();
+                result += optStack.pop();
+                number = 0;
+            }
+        }
+
+        return result + number * signValue;
+    }
+}
+
+
+
+
+
+
 // Split a String Into the Max Number of Unique Substrings
 
 class Solution {
