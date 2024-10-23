7647ef1e90210f488724957e9ed781ff4e5741fb
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-23 09:56:52 +0530

    Update DFS_BFS.java

--- DFS_BFS.java
+++ DFS_BFS.java
@@ -1,3 +1,55 @@
+// Course Schedule
+
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        int[] inDegree = new int[numCourses];
+        List<List<Integer>> adjList = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int[] prerequisite : prerequisites) {
+
+            int course = prerequisite[0];
+            int pre = prerequisite[1];
+
+            adjList.get(pre).add(course);
+            inDegree[course]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int count = 0;
+        while (!queue.isEmpty()) {
+
+            int current = queue.poll();
+            count++;
+
+            for (int neighbor : adjList.get(current)) {
+                inDegree[neighbor]--;
+
+                if (inDegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+}
+
+
+
 // Kth Largest Sum in a Binary Tree
 
 class Solution {
