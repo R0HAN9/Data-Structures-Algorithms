a1b427a7e1e50346a409335e5020e98d9f38497d
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-23 10:15:47 +0530

    Update Heap(PriorityQueue).java

--- Heap(PriorityQueue).java
+++ Heap(PriorityQueue).java
@@ -36,3 +36,41 @@ class Solution {
         return ans;
     }
 }
+
+
+
+// Find Median from Data Stream
+
+
+
+class MedianFinder {
+
+    private PriorityQueue<Integer> lowerHalf;
+    private PriorityQueue<Integer> upperHalf;
+
+    public MedianFinder() {
+        
+        lowerHalf = new PriorityQueue<>((a,b) -> b-a);
+        upperHalf = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        
+        lowerHalf.offer(num);
+        upperHalf.offer(lowerHalf.poll());
+
+        if (lowerHalf.size() < upperHalf.size()) {
+            lowerHalf.offer(upperHalf.poll());
+        }
+    }
+    
+    public double findMedian() {
+        
+        if (lowerHalf.size() > upperHalf.size()) {
+            return lowerHalf.peek();
+        }
+        else {
+            return (lowerHalf.peek() + upperHalf.peek()) / 2.0;
+        }
+    }
+}
