4dfbe268f842636ce3cd0b2ff24d44d93508009f
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-24 10:40:30 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,3 +1,32 @@
+// Longest Consecutive Sequence
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        
+        int maxSeq = 0;
+        Set<Integer> sequences = new HashSet<>();
+
+        for (int num : nums) {
+            sequences.add(num);
+        }
+
+        for (int num : nums) {
+
+            int nextInSeq = num + 1;
+            int prevInSeq = num - 1;
+            int currentSequence = 1;
+
+            while (sequences.remove(prevInSeq--)) currentSequence++;
+            while (sequences.remove(nextInSeq++)) currentSequence++;
+
+            if (currentSequence > maxSeq) maxSeq = currentSequence;
+
+        }
+        return maxSeq;
+    }
+
+
+
 // Game of Life
 
 
