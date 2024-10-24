8878d047ed1cc1cff86531bb0e54b32448864dd7
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-24 10:20:17 +0530

    Update Greedy.java

--- Greedy.java
+++ Greedy.java
@@ -91,3 +91,21 @@ class Solution {
         return num;
     }
 }
+
+
+// Jump Game
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+}
