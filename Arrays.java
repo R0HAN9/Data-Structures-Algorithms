9784a20ae5906cb21e80335ba93f51cc484d1c7c
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-25 11:17:23 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,3 +1,33 @@
+// Longest Increasing Subsequence
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        
+        int[] tails = new int[nums.length];
+        int size = 0;
+
+        for (int x : nums) {
+            int i = 0;
+            int j = size;
+
+            while (i != j) {
+                int m = (i + j) / 2;
+
+                if (tails[m] < x) i = m + 1;
+                else j = m;
+            }
+
+            tails[i] = x;
+            if (i == size) ++size;
+        }
+
+        return size;
+    }
+}
+
+
+
+
 // Longest Consecutive Sequence
 
 class Solution {
