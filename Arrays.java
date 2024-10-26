34f82c0d25564eda3187372bc350c5e25b5b08cb
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-26 10:30:12 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,3 +1,23 @@
+// Remove Element
+
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;        
+    }
+}
+
+
+
 // Median of Two Sorted Arrays
 
 
