0fc25709fd72002ca7308099aebc2fc4e6b6904a
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-26 11:00:34 +0530

    Update ModifiedBinarySearch.java

--- ModifiedBinarySearch.java
+++ ModifiedBinarySearch.java
@@ -1,3 +1,55 @@
+// Height of Binary Tree After Subtree Removal Queries
+
+
+class Solution {
+
+    int[] depth;
+    int[] levelArray;
+    int[] max1;
+    int[] max2;
+
+    public int height(TreeNode root, int level) {
+
+        if (root == null) return 0;
+        levelArray[root.val] = level;
+        depth[root.val] = 1 + Math.max(height(root.left, level + 1), height(root.right, level + 1));
+
+        if (max1[level] < depth[root.val]) {
+
+            max2[level] = max1[level];
+            max1[level] = depth[root.val];
+        }
+        else if (max2[level] < depth[root.val]) {
+            max2[level] = depth[root.val];
+        }
+
+        return depth[root.val];
+    }
+
+    public int[] treeQueries(TreeNode root, int[] queries) {
+        
+        depth = new int[100001];
+        levelArray = new int[100001];
+        max1 = new int[100001];
+        max2 = new int[100001];
+
+        height(root, 0);
+        for (int i = 0; i < queries.length; i++) {
+
+            int q = queries[i];
+            int level = levelArray[q];
+
+            queries[i] = (max1[level] == depth[q] ? max2[level] : max1[level]) + level - 1;
+
+        }
+
+        return queries;
+    }
+}
+
+
+
+
 // Search in Rotated Sorted Array ***
 
 
