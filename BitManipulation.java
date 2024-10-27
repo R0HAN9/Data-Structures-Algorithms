2e00ec14649b6f3c49f50d4a96e8455b070d1fb9
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-27 12:06:02 +0530

    Update BitManipulation.java

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,3 +1,22 @@
+// Bitwise AND of Numbers Range
+
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        
+        int count = 0;
+        while (left != right) {
+
+            left >>= 1;
+            right >>= 1;
+            count++;
+        }
+
+        return (left << count);
+    }
+}
+
+
+
 // Count Number of Maximum Bitwise-OR Subsets
 
 class Solution {
