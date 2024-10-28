a35ee6874b6c1aba8cc02f81beb22ff47b57db47
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-28 10:22:15 +0530

    Update Depth-FirstSearch(DFS).java

--- Depth-FirstSearch(DFS).java
+++ Depth-FirstSearch(DFS).java
@@ -108,3 +108,39 @@ class Solution {
                 (checker(node1.right, node2.right) || checker(node1.right, node2.left)); 
     }
 }
+
+
+
+// Construct Binary Tree from Inorder and Postorder Traversal
+
+class Solution {
+    public TreeNode buildHelper(int[] inorder, int inorderStart, int inorderEnd, 
+                                 int[] postorder, int postorderStart, int postorderEnd, 
+                                 Map<Integer, Integer> indexMap) {
+        if (postorderStart > postorderEnd || inorderStart > inorderEnd) {
+            return null;
+        }
+
+        TreeNode rootNode = new TreeNode(postorder[postorderEnd]);
+        int rootIndexInInorder = indexMap.get(rootNode.val);
+        int leftSubtreeSize = rootIndexInInorder - inorderStart;
+
+        rootNode.left = buildHelper(inorder, inorderStart, rootIndexInInorder - 1, 
+                                     postorder, postorderStart, postorderStart + leftSubtreeSize - 1, 
+                                     indexMap);
+        rootNode.right = buildHelper(inorder, rootIndexInInorder + 1, inorderEnd, 
+                                      postorder, postorderStart + leftSubtreeSize, postorderEnd - 1, 
+                                      indexMap);
+
+        return rootNode;
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            indexMap.put(inorder[i], i);
+        }
+
+        return buildHelper(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1, indexMap);
+    }
+}
