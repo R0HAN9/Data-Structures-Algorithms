c1f2757ec9e16e2b3f6e153d81a94c1113fab10d
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-28 10:27:59 +0530

    Update BinaryTreeTraversal.java

--- BinaryTreeTraversal.java
+++ BinaryTreeTraversal.java
@@ -1,4 +1,18 @@
- // Binary Tree Maximum Path Sum
+// Path Sum
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return root.val == targetSum;
+
+        int remainingSum = targetSum - root.val;
+        return hasPathSum(root.left, remainingSum) || hasPathSum(root.right, remainingSum);
+    }
+}
+
+
+// Binary Tree Maximum Path Sum
 
 
 class Solution {
