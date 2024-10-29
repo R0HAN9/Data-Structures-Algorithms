2d08290ec0ac9651e18e872f8dff85fd98e4d2ea
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-29 10:35:38 +0530

    Update MatrixTraversal.java

--- MatrixTraversal.java
+++ MatrixTraversal.java
@@ -1,3 +1,65 @@
+// Surrounded Regions
+
+
+class Solution {
+    public void solve(char[][] board) {
+        
+        int n = board.length;
+        int m = board[0].length;
+
+        int[] dRow = {-1, 0, 1, 0};
+        int[] dCol = {0, 1, 0, -1};
+        boolean[][] vis = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i][0] && board[i][0] == 'O') {
+                dfs(i, 0, dRow, dCol, vis, board);
+            }
+
+            if (!vis[i][m - 1] && board[i][m - 1] == 'O') {
+                dfs(i, m - 1, dRow, dCol, vis, board);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (!vis[0][i] && board[0][i] == 'O') {
+                dfs(0, i, dRow, dCol, vis, board);
+            }
+
+            if (!vis[n - 1][i] && board[n - 1][i] == 'O') {
+                dfs(n - 1, i, dRow, dCol, vis, board);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (!vis[i][j] && board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    public void dfs(int row, int col, int[] dRow, int[] dCol, boolean[][] vis, char[][] board) {
+
+        vis[row][col] = true;
+        int n = board.length;
+        int m = board[0].length;
+
+        for (int i = 0; i < 4; i++) {
+            int newRow = row + dRow[i];
+            int newCol = col + dCol[i];
+
+            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && !vis[newRow][newCol] && board[newRow][newCol] == 'O') { 
+                dfs(newRow, newCol, dRow, dCol, vis, board);
+            }
+        }
+    }
+}
+
+
+
 // Number of Islands
 
 class Solution {
