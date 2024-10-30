cacb25f6bb01b3fddbfe9cd73efc74e1dfdaa3fd
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-30 10:36:39 +0530

    Update TwoPointers.java

--- TwoPointers.java
+++ TwoPointers.java
@@ -148,3 +148,55 @@ class Solution {
         }
     }
 }
+
+
+// Sort List
+
+
+class Solution {
+    private ListNode mid(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode mergeSort(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode mid = mid(head);
+        ListNode newNode = mid.next;
+        mid.next = null;
+
+        ListNode leftHalf = sortList(head);
+        ListNode rightHalf = sortList(newNode);
+
+        return mergeSort(leftHalf, rightHalf);
+    }
+}
