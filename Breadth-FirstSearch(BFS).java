802b4831d8d0bc9f35e1ed0af4cabd67714e7f84
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-30 10:13:42 +0530

    Update Breadth-FirstSearch(BFS).java

--- Breadth-FirstSearch(BFS).java
+++ Breadth-FirstSearch(BFS).java
@@ -134,3 +134,30 @@ class Solution {
         return 0;
     }
 }
+
+
+
+// Clone Graph
+
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        Map<Node, Node> oldToNew = new HashMap<>();
+        return dfs(node, oldToNew);
+    }
+
+    private Node dfs(Node node, Map<Node, Node> oldToNew) {
+        if (oldToNew.containsKey(node)) return oldToNew.get(node);
+
+        Node copy = new Node(node.val);
+        oldToNew.put(node, copy);
+
+        for (Node neighbor : node.neighbors) {
+            copy.neighbors.add(dfs(neighbor, oldToNew));
+        }
+
+        return copy;
+    }    
+}
