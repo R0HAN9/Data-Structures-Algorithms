00c581d05b570a6e66ad2bc8ed61698b4f589ee2
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-31 10:07:08 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -67,6 +67,33 @@ class Solution {
     }
 }
 
+
+// Find Minimum in Rotated Sorted Array
+
+
+class Solution {
+    public int findMin(int[] nums) {
+        
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return nums[left];
+    }
+}
+
+
+
 // Median of Two Sorted Arrays
 
 
