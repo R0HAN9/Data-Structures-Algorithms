95f3da577f484280b4383896321406b25e124190
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-31 10:16:53 +0530

    Update Heap(PriorityQueue).java

--- Heap(PriorityQueue).java
+++ Heap(PriorityQueue).java
@@ -68,7 +68,29 @@ class Solution {
     }
 }
 
+// Maximum Sum Circular Subarray
 
+
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        
+        int totalSum = 0, currentMax = 0, currentMin = 0;
+        int maxSum = Integer.MIN_VALUE, minSum = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            totalSum += num;
+
+            currentMax = Math.max(currentMax + num, num);
+            maxSum = Math.max(maxSum, currentMax);
+
+            currentMin = Math.min(currentMin + num, num);
+            minSum = Math.min(minSum, currentMin);
+        }
+
+        if (maxSum < 0) return maxSum;
+        return Math.max(maxSum, totalSum - minSum);
+    }
+}
 
 // Find Median from Data Stream
 
