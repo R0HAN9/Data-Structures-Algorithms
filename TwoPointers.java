74ce99b37710e5e70f1736164328bcc3dfa1b8e0
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-31 10:23:33 +0530

    Update TwoPointers.java

--- TwoPointers.java
+++ TwoPointers.java
@@ -1,3 +1,24 @@
+// Remove Duplicates from Sorted Array
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        if (nums.length == 0) return 0;
+        int idx = 1;
+
+        for (int j = 1; j < nums.length; j++) {
+            if(nums[j] != nums[idx - 1]) {
+
+                nums[idx] = nums[j];
+                idx++;
+            }
+        }
+
+        return idx;
+    }
+}
+
+
 // Container With Most Water
 
 
