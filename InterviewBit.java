68f894606e9d530e320c0c2682293fd3ad3917b1
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-11-04 15:14:06 +0530

    Update InterviewBit.java

--- InterviewBit.java
+++ InterviewBit.java
@@ -108,3 +108,30 @@ public class Solution {
         return A%9;
     }        
 }   
+
+
+
+// Number of Sundays
+
+public class Solution {
+    public int solve(String A, int B) {
+        int day=0;
+        switch(A)
+        {
+        case "Sunday": day=6;
+        break;
+        case "Tuesday": day=1;
+        break;
+        case "Wednesday": day=2;
+        break;
+        case "Thursday": day=3;
+        break;
+        case "Friday": day=4;
+        break;
+        case "Saturday": day=5;
+        break;
+        }
+        int count =(B+day)/7;
+        return count;
+    }
+}
