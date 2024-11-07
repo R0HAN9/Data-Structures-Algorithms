9264708a5ff412f1d2a71ce372888d26825336d1
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-11-07 13:59:01 +0530

    Update InterviewBit.java

--- InterviewBit.java
+++ InterviewBit.java
@@ -1,4 +1,49 @@
 
+// Flipping String
+
+public class Solution {
+    public int solve(String A) {
+        ArrayList<String> alist=new ArrayList<>();
+        int one=0;
+        int zero=0;
+        for(int i=0;i<A.length();i++){
+            while(i<A.length() && A.charAt(i)=='1'){
+                one++;
+                i++;
+            }
+            if(one>0){
+                alist.add(""+one);
+                one=0;
+            }
+            while(i<A.length() && A.charAt(i)=='0'){
+                zero++;
+                i++;
+            }
+            if(zero>0){
+                alist.add(zero+"o");
+                zero=0;
+            }
+            i=i-1;
+        }
+        int max=0;
+        for(int i=0;i<alist.size();i++){
+            int count=0;
+            if(alist.get(i).charAt(alist.get(i).length()-1)=='o'){
+                count+=Integer.valueOf(alist.get(i).substring(0,alist.get(i).length()-1));
+                if(i!=0){
+                    count+=Integer.valueOf(alist.get(i-1).substring(0,alist.get(i-1).length()));
+                }
+                if(i!=alist.size()-1){
+                    count+=Integer.valueOf(alist.get(i+1).substring(0,alist.get(i+1).length()));
+                }
+            }
+            max=Math.max(count,max);
+        }
+        return max;
+    }
+}
+
+
 // Queries in Infinite String!
 
 import java.util.ArrayList;
