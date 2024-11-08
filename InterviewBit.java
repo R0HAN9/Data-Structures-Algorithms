817fddc9fe0ec0571b14c432d65a6b0ee8d09cb1
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-11-08 17:39:19 +0530

    Update InterviewBit.java

--- InterviewBit.java
+++ InterviewBit.java
@@ -1,3 +1,27 @@
+// Palindromic Words
+
+public class Solution {
+    public int solve(String A) {
+        String[] arr = A.split(" ");
+        int count = 0;
+        for(String x : arr ) {
+            count += isPalindrome(x);
+        }
+        return count;    
+    }
+   
+    public static int isPalindrome(String str)
+    {
+        for (int i = str.length() - 1,j=0; j<i; i--,j++) {
+           if (str.charAt(i) != str.charAt(j)){
+               return 0;
+           }
+        }
+        return 1;
+    }
+}
+
+
 // Valid Password
 
 public class Solution {
