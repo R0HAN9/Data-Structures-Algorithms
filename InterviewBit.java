8538b67dacd0c52215849b6c0e3a0f8bc32caaae
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-11-08 17:37:31 +0530

    Update InterviewBit.java

--- InterviewBit.java
+++ InterviewBit.java
@@ -1,3 +1,18 @@
+// String Inversion
+
+public class Solution {
+    public String solve(String A) {
+        
+        char[]arr=A.toCharArray();
+        
+        for(int i=0;i<arr.length;i++){
+            arr[i]=(char)(arr[i]^32);
+        }
+        return new String(arr);
+    }
+}
+
+
 // Character Frequencies
 
 public class Solution {
