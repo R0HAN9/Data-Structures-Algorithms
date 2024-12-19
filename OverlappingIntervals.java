d713a9718f86e5552c1de39a817217e8745e7078
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-12-19 06:33:19 +0530

    Update OverlappingIntervals.java

--- OverlappingIntervals.java
+++ OverlappingIntervals.java
@@ -3,22 +3,28 @@
 
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        int res = 0;
+        int res = 0; // To count the number of overlapping intervals to remove
 
+        // Sort intervals by their end times in ascending order
         Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
-        int prev_end = intervals[0][1];
 
+        int prev_end = intervals[0][1]; // Initialize with the end time of the first interval
+
+        // Iterate through the intervals starting from the second one
         for (int i = 1; i < intervals.length; i++) {
+            // Check if the current interval overlaps with the previous one
             if (prev_end > intervals[i][0]) {
-                res++;
+                res++; // Increment the count of intervals to remove
             } else {
+                // Update the previous end time if there's no overlap
                 prev_end = intervals[i][1];
             }
         }
 
-        return res;        
+        return res; // Return the number of intervals removed
     }
 }
+
 
 
 
